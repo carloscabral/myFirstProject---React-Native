561016e38774fea908945def0630438efc755d90
Carlos Cabral <carlos.gomescabral@gmail.com>
2015-12-28 18:14:11 -0200

    Added A Text Input Component That Modifies The Content Of A Label While Typing

--- index.ios.js
+++ index.ios.js
@@ -9,15 +9,29 @@ var {
   View,
   TouchableHighlight,
   AlertIOS,
+  TextInput,
 } = React;
 
 var Tableless = React.createClass({
 
+  getInitialState : function() {
+    return {
+      myText : "Hello, Tableless!"
+    };
+  },
+
+  textInputDidChange : function (event) {
+    this.setState({ myText: event.nativeEvent.text });
+  },
+
   render: function() {
     return (
       <View style={styles.container} >
+        <TextInput style = {{ height: 50, padding: 6, fontSize: 16, borderColor: "lightblue", borderWidth: 1, margin: 10, borderRadius: 4 }}
+                   placeholder="Type something..."
+                   onChange={this.textInputDidChange} />
         <Text style={styles.myText}>
-          Hello, Tableless!
+          {this.state.myText}
         </Text>
         <TouchableHighlight onPress={() => AlertIOS.alert(
             'Simple Title',
